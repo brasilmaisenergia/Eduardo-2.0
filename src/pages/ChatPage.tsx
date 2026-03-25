b754a0ab828a1d64fece523baeecf64965079f73
Brasil Mais Energia <bme@brasilmaisenergia.com>
2026-03-25 15:57:29 -0300

    Create ChatPage.tsx

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,12 @@
+/**
+ * Chat Page
+ * Page para acessar o chat com Eduardo
+ */
+
+import { ChatEduardo } from '../components/ChatEduardo'
+
+export function ChatPage() {
+    return <ChatEduardo />
+}
+
+export default ChatPage
